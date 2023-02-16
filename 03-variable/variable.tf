bb4ff5f9e3deccd3f2c6f3ef79bc9c4518ab1200
Manikannan Marimuthu <manikannanmarimuthu@gmail.com>
2023-02-16 22:26:52 +0530

    updating ./learn-terraform

--- 03-variable/variable.tf
+++ 03-variable/variable.tf
@@ -5,3 +5,43 @@ variable "sample" {
 output "sample" {
   value = var.sample
 }
+#terraform supports String
+variable "sample2" {
+  default = "String should be double quotes"
+}
+
+output "sample2" {
+  value = var.sample2
+}
+
+variable "sample3" {
+  default = true
+}
+
+output "sample3" {
+  value = var.sample3
+}
+
+variable "sample4" {
+  default = [
+  100,
+  "abc",
+  false]
+}
+
+output "sample4" {
+  value = var.sample4[1]
+}
+
+
+variable "sample5" {
+  default = {
+    number = 100
+    string= "xyz"
+    boolean = false
+  }
+}
+
+output "sample5" {
+  value = var.sample5["string"]
+}
