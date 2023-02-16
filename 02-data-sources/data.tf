0ba48b53ec51553ce3de1e26fea9252bf983dcce
Manikannan Marimuthu <manikannanmarimuthu@gmail.com>
2023-02-16 21:52:36 +0530

    updating ./learn-terraform

--- 02-data-sources/data.tf
+++ 02-data-sources/data.tf
@@ -7,3 +7,11 @@ data "aws_ami" "example" {
 output "ami" {
   value = data.aws_ami.example
 }
+
+data "aws_instance" "foo" {
+  instance_id = "i-023545e4c2397f0dc"
+}
+
+output "instance" {
+  value = data.aws_instance.foo.public_ip
+}
